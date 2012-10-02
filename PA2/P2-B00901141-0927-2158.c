0924c8953cde8562aea8acef79166bd708c27b7d
khdreamer <khdreamer@khdreamer-VirtualBox.(none)>
2012-10-02 13:44:55 +0800

    ubuntu mac

---
+++ PA2/P2-B00901141-0927-2158.c
@@ -0,0 +1,89 @@
+/*
+** server.c -- a stream socket server demo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define MYPORT 12016    // the port users will be connecting to
+#define BACKLOG 10     // how many pending connections queue will hold
+
+int main(void)
+{
+   
+    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+    struct sockaddr_in my_addr;    // my address information
+    struct sockaddr_in their_addr; // connector's address information
+    int sin_size;
+    int yes=1;
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+        perror("setsockopt");
+        exit(1);
+    }
+    
+    my_addr.sin_family = AF_INET;         // host byte order
+    my_addr.sin_port = htons(MYPORT);     // short, network byte order
+    my_addr.sin_addr.s_addr = INADDR_ANY; // automatically fill with my IP
+    memset(&(my_addr.sin_zero), '\0', 8); // zero the rest of the struct
+
+    if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)) == -1) {
+        perror("bind");
+        exit(1);
+    }
+
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+    while(1) {  // main accept() loop
+
+	char bufferStr[80];
+        sin_size = sizeof(struct sockaddr_in);
+        if ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size)) == -1) {
+            perror("accept");
+            continue;
+        }
+        printf("server: got connection from %s\n",inet_ntoa(their_addr.sin_addr));
+
+	//read
+	if(read(new_fd, bufferStr, 79)<0)
+	  perror("read");
+
+	//calculate correct length
+	int length=0;
+	while(bufferStr[length]!='\r'){
+
+	  length++;
+
+	}
+
+	//get (prettier) result
+	char resultStr[length+1];
+	strncpy(resultStr, bufferStr, length);
+	strcat(resultStr,"\n");	
+
+	int sendResult=send(new_fd, resultStr, length+1, 0);
+	
+	if (sendResult == -1)
+	  perror("send");
+
+	close(new_fd);
+    }
+
+    return 0;
+}
+
